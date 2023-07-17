c3b803c8ce6db7d91bc174020460eb0b0957b634
Jose Abreu <jose.abreu@cursor.cl>
2023-07-17 15:53:53 -0400

    Contact Form template creation.

---
+++ src/components/ContactForm.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+
+const ContactForm = () => {
+	const [status, setStatus] = useState('Submit');
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		setStatus('Sending...');
+		const { name, email, message } = e.target.elements;
+		let details = {
+			name: name.value,
+			email: email.value,
+			message: message.value,
+		};
+		let response = await fetch('http://localhost:5000/contact', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json;charset=utf-8',
+			},
+			body: JSON.stringify(details),
+		});
+		setStatus('Submit');
+		let result = await response.json();
+		alert(result.status);
+	};
+	return (
+		<form onSubmit={handleSubmit}>
+			<div>
+				<label htmlFor='name'>Name:</label>
+				<input type='text' id='name' required />
+			</div>
+			<div>
+				<label htmlFor='phone'>Phone Number:</label>
+				<input type='number' id='phone-number' required />
+			</div>
+			<div>
+				<label htmlFor='email'>Email:</label>
+				<input type='email' id='email' required />
+			</div>
+			<div>
+				<label htmlFor='message'>Message:</label>
+				<textarea id='message' required />
+			</div>
+			<button type='submit'>{status}</button>
+		</form>
+	);
+};
+
+export default ContactForm;

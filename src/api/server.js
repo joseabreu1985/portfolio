9ecedf966ad0f05779b94a8a3bf73a214c461af5
joseabreu1985 <josearmandoabreu.85@gmail.com>
2023-07-24 13:04:36 -0400

    test

--- src/api/server.js
+++ src/api/server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/', router);
-app.listen(8080, () => console.log('Server Running'));
+app.listen(8080, () => console.log('Server Running...'));
 
 const contactEmail = nodemailer.createTransport({
 	service: 'gmail',
